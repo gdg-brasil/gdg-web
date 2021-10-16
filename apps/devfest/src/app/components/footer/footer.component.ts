713dd91c7475ff0c064e9e9e803abbc46679972f
Fernando de Moraes Sedrez <fmsedrez@gmail.com>
2021-10-16 12:34:25 -0300

    Add footer

--- apps/devfest/src/app/components/footer/footer.component.ts
+++ apps/devfest/src/app/components/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { faAd } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'dfb-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  faAd = faAd
+}
