d755138037b397af422ee3fb09e9061feb67bb5f
Fernando de Moraes Sedrez <fmsedrez@gmail.com>
2021-10-16 12:47:43 -0300

    Fix error

--- apps/devfest/src/app/components/footer/footer.component.ts
+++ apps/devfest/src/app/components/footer/footer.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-import { faAd } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'dfb-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-  faAd = faAd
-}
