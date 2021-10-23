5756b95b70b51eaa719b7c86769a6bdf64ecf597
Antonio Pedro Ferreira <apsf88@gmail.com>
2021-10-23 15:59:34 -0300

    corrigindo a url base

--- apps/devfest/src/environments/environment.prod.ts
+++ apps/devfest/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backoffice.devfest.com.br/wp-json/wp/v2'
+  // apiUrl: 'https://backoffice.devfest.com.br/wp-json/wp/v2'
+  apiUrl: 'https://backoffice.devfest.com.br/wp-json/acf/v3'
 };
