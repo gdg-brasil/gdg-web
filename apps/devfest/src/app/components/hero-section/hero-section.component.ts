88431c3a7ce6e9216dbbe84ea4d2f736a2a53c39
Fernando Sedrez <fmsedrez@gmail.com>
2022-10-17 23:55:55 -0300

    Feat devfest 2022

--- apps/devfest/src/app/components/hero-section/hero-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'dfb-hero-section',
-  templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss'],
-})
-export class HeroSectionComponent {}
--- apps/io-extended/src/app/components/subscription-section/subscription-section.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'iox-subscription-section',
-  templateUrl: './subscription-section.component.html'
-})
-export class SubscriptionSectionComponent {}
